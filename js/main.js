0fcf4d99555a36f98ab90563223fe387188e4d11
Xabier Maeztu <trisketonni@tramankulu.(none)>
2013-06-25 12:01:46 +0200

    Adding xml

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 $(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-	    var target = this.hash,
-	    $target = $(target);
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+  //Honako kodea smooth scrolling egiten du
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+    var target = this.hash,
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+  
+  $(function() {
+    var language = 'eu';
+    $.ajax({
+        url: 'translations.xml',
+        success: function(xml) {
+            $(xml).find('translation').each(function(){
+                var id = $(this).attr('id');
+                var text = $(this).find(language).text();
+                $("." + id).html(text);
+            });
+        }
+    });
+});
 });
