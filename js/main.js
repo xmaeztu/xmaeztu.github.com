8a6e3818e7ec686b1caa688063911669cedecd79
Xabier <trisketonni@gmail.com>
2013-07-02 16:59:08 +0200

    Translations, scroll and styles

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
     var target = this.hash,
     $target = $(target);
     $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
-    }, 900, 'swing', function () {
+      'scrollTop': $target.offset().top - 70
+    }, 1200, 'swing', function () {
       window.location.hash = target;
     });
   });
